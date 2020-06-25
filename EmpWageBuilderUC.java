993298b7394d5536d49ae8ec6fbea58c82421767
hemali <hemalidodia1310@gmail.com>
2020-06-25 15:07:20 +0530

    while loop added, total working days added

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -15,22 +15,35 @@ public class EmpWageBuilderUC
 		int WagePerHour = 20;
 		byte FullDayHour = 8;
 		byte HalfDayHour = 4;
-		int EmpWageFullfDay = 0;
+		int EmpWageFullDay = 0;
 		int EmpWageHalfDay = 0;
-		double WorkingTime = Math.floor(Math.random() * 10) % 2;
-		int Day = (int) WorkingTime;
-		switch(Day)
+		int WorkingDays = 20;
+		int totalWage = 0;
+		int Wage = 0;
+
+		while(WorkingDays >= 0)
 		{
-		case 0:
-			EmpWageHalfDay = WagePerHour * HalfDayHour;
-        	        System.out.println("employee wage = "+EmpWageHalfDay);
-			break;
-        	case 1:
-			EmpWageFullfDay = WagePerHour * FullDayHour;
-        	        System.out.println("employee wage = "+EmpWageFullfDay);
-			break;
-		default:
-			System.out.println("employee wage = 0");
-			break;
+			double WorkingTime = Math.floor(Math.random() * 10) % 2;
+			int Day = (int) WorkingTime;
+			switch(Day)
+			{
+			case 0:
+				EmpWageHalfDay = WagePerHour * HalfDayHour;
+				Wage = Wage + EmpWageHalfDay;
+        	        	//System.out.println("employee wage = "+EmpWageHalfDay);
+				break;
+        		case 1:
+				EmpWageFullDay = WagePerHour * FullDayHour;
+				Wage = Wage + EmpWageFullDay;
+        	        	//System.out.println("employee wage = "+EmpWageFullfDay);
+				break;
+			default:
+				System.out.println("employee wage = 0");
+				break;
+			}
+			WorkingDays--;
+			totalWage = totalWage + Wage;
+		}
+	System.out.println("monthly wage = "+totalWage);
 	}
 }
