9dc1e3951ff46e2cc93dc3a7b0544d54d1a3041b
hemali <hemalidodia1310@gmail.com>
2020-06-25 14:44:19 +0530

    check employee is present or not

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -3,5 +3,13 @@ public class EmpWageBuilderUC
 	public static void main(String[] agrs)
 	{
 		System.out.println("calculate employee wage");
+		//constants
+		int IS_FULL_TIME = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("present");
+		else
+			System.out.println("absent");
 	}
 }
