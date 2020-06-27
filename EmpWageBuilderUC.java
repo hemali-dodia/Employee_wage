1711b1735d922bf7b814d379546ffb0b16b8460c
hemali <hemalidodia1310@gmail.com>
2020-06-27 10:03:18 +0530

    uc4 updated

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -4,33 +4,42 @@ public class EmpWageBuilderUC
 	{
 		System.out.println("calculate employee wage");
 		//constants
-		int IS_FULL_TIME = 1;
+		int is_present = 1;
+		int empWage = 0;
+		int wagePerHour = 20;
+                byte fullDayHour = 8;
+                byte halfDayHour = 4;
+                int empWageFullfDay = 0;
+                int empWageHalfDay = 0;
+                double workingTime = Math.floor(Math.random() * 10) % 2;
+                int day = (int) workingTime;
+
 		//Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("present");
-		else
-			System.out.println("absent");
-
-		int WagePerHour = 20;
-		byte FullDayHour = 8;
-		byte HalfDayHour = 4;
-		int EmpWageFullfDay = 0;
-		int EmpWageHalfDay = 0;
-		double WorkingTime = Math.floor(Math.random() * 10) % 2;
-		int Day = (int) WorkingTime;
-		switch(Day)
+		if (empCheck == is_present)
 		{
-		case 0:
-			EmpWageHalfDay = WagePerHour * HalfDayHour;
-        	        System.out.println("employee wage = "+EmpWageHalfDay);
-			break;
-        	case 1:
-			EmpWageFullfDay = WagePerHour * FullDayHour;
-        	        System.out.println("employee wage = "+EmpWageFullfDay);
-			break;
-		default:
-			System.out.println("employee wage = 0");
-			break;
+			System.out.println("present");
+			switch(day)
+                	{
+                	case 0:
+                        	empWageHalfDay = wagePerHour * halfDayHour;
+                        	System.out.println("employee wage = "+empWageHalfDay);
+                        	break;
+               		case 1:
+                        	empWageFullfDay = wagePerHour * fullDayHour;
+                        	System.out.println("employee wage = "+empWageFullfDay);
+                        	break;
+                	default:
+                        	System.out.println("employee wage = 0");
+                        	break;
+			}
+		}
+		else
+		{
+
+			empWage = 0;
+			System.out.println("absent, emp wage - "+empWage);
+		}
+
 	}
 }
